a3260a86b53c679cbf3d637fee6fbbe500843e84
AjayKumarKota <kotaajaykumar87@gmail.com>
2015-09-03 10:31:22 +0530

    Initial Commit

---
+++ example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  example
+//
+//  Created by ajaykumar on 03/09/15.
+//  Copyright (c) 2015 AjayKumarKota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  example
+//
+//  Created by ajaykumar on 03/09/15.
+//  Copyright (c) 2015 AjayKumarKota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
